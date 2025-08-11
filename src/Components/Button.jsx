2265a6d1f4f5c37da857aff75d7c9c4e5595561e
Pheletso8 <pheletso.marumoloe8@gmail.com>
2025-08-11 09:34:16 +0200

    main

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 function Button({text, onAdd}){
   
   return (
-    <div className='h-8 rounded transition delay-150 duration-300 ease-in-out hover:-translate-x+0.5 hover:scale-110'
+    <button className='h-8 rounded transition delay-150 duration-300 ease-in-out hover:-translate-x+0.5 hover:scale-110'
      style={{backgroundColor:'green'}} 
-     onClick={onAdd} >
-        <button>{text}</button>
-    </div>
-  )
+     onClick={onAdd} type='button'>
+      {text}
+    </button>
+  );
 }
 
 export default Button
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Button from './Button';
 
-function Header({onAdd}){
-  const text = 'Add';
+function Header({onAdd, showAddTask}){
+  const text = showAddTask ? 'Close' : 'Add';
   return (
     <div className='flex w-full mt-5'>
         <div className='font-sans font-bold text-3xl pl-4'><h1>Task Tracker</h1></div>
