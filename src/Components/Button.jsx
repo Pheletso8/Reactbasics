f0452d969e42e3225c4e8d3001d48abe9ee8b5c8
Pheletso8 <pheletso.marumoloe8@gmail.com>
2025-08-11 09:34:58 +0200

    main

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Button({text, onAdd}){
   
   return (
-    <button className='h-8 rounded transition delay-150 duration-300 ease-in-out hover:-translate-x+0.5 hover:scale-110'
+    <button className='h-8 rounded transition delay-150 duration-300 ease-in-out hover:-translate-x-1 hover:scale-110'
      style={{backgroundColor:'green'}} 
      onClick={onAdd} type='button'>
       {text}
